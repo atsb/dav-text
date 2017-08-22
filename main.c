4dac86aa1da5d5ee31a1cc67e4a9852e06069e4f
Gibbon <abilbrou@gmail.com>
2017-08-22 13:14:51 +0000

    Lets keep it at 0.8.6 for now, it's a tiny change.

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 int maxY,maxX;
 int helpBarUpdate=0;
 
-char *version = "Dav - 0.8.7\n\nCopyright 2001-2003 David Gucwa\nCopyright 2017 Adam Bilbrough\n";
+char *version = "Dav - 0.8.6\n\nCopyright 2001-2003 David Gucwa\nCopyright 2017 Adam Bilbrough\n";
 
 /* Preferences and default values */
 int maxUndoLength = 500;
