5d136ab98411c367b3f5e382cb3792e9c723ff4f
Adam <44937323+atsb@users.noreply.github.com>
2019-11-10 11:10:38 +0100

    Bump version due to debian changes

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 int maxY,maxX;
 int helpBarUpdate=0;
 
-char *version = "Dav - 0.8.9\n\nCopyright 2001-2003 David Gucwa\nCopyright 2017-2019 Adam Bilbrough";
+char *version = "Dav - 0.9.0\n\nCopyright 2001-2003 David Gucwa\nCopyright 2017-2019 Adam Bilbrough";
 char *license = "This program is free software under the GPL version 2 license.";
                 
 /* Preferences and default values */
